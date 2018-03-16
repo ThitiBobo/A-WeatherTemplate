b78e9335314d7fd07f6c8d9a2900e603628872cb
Thibaut Delplanque <t.delplanque92@gmail.com>
2018-03-16 12:11:07 +0100

    ajout de la classe WeatherTask

--- app/src/main/java/fr/iut_amiens/weatherapplication/WeatherTask.java
+++ app/src/main/java/fr/iut_amiens/weatherapplication/WeatherTask.java
@@ -1,8 +1,36 @@
 package fr.iut_amiens.weatherapplication;
 
+import android.os.AsyncTask;
+
 /**
  * Created by canard on 16/03/18.
  */
 
-public class WeatherTask {
+public class WeatherTask extends AsyncTask {
+
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+    }
+
+    @Override
+    protected void onProgressUpdate(Object[] values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onCancelled() {
+        super.onCancelled();
+    }
 }
