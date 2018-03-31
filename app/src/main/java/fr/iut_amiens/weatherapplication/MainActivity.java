1c02a4b65171e1a20d107030d927a274e6ad6202
Thibaut Delplanque <t.delplanque92@gmail.com>
2018-03-31 17:18:18 +0200

    ajout d'un floating button

--- app/src/main/java/fr/iut_amiens/weatherapplication/MainActivity.java
+++ app/src/main/java/fr/iut_amiens/weatherapplication/MainActivity.java
@@ -1,9 +1,12 @@
 package fr.iut_amiens.weatherapplication;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import fr.iut_amiens.weatherapplication.openweathermap.WeatherManager;
 
@@ -20,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
 
         configureRecyclerView();
         mWeatherTask = new WeatherTask((WeatherAdapter) mRecyclerView.getAdapter());
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         mWeatherTask.execute();
 
